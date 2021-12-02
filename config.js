7327dc03ba73f61ddf867791865db8f6bc36571b
shaurya <shauryagupta0305@gmail.com>
2021-12-02 13:09:25 +0530

    E-Ride1

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBh5TSMtFHl2U9oQtG3OSCSFq9PumTkAa4",
+    authDomain: "voting-app-5a16a.firebaseapp.com",
+    databaseURL: "https://voting-app-5a16a-default-rtdb.firebaseio.com",
+    projectId: "voting-app-5a16a",
+    storageBucket: "voting-app-5a16a.appspot.com",
+    messagingSenderId: "664130284937",
+    appId: "1:664130284937:web:d5464ecf3e77b4b891c558"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
